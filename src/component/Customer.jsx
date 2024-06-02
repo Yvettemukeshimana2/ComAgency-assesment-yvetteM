969ae3e594303e07d96c3b819b0642807e9a43f1
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2024-06-02 18:07:08 +0200

    fetch2

---
+++ src/component/Customer.jsx
@@ -0,0 +1,119 @@
+ import React, { useState, useEffect } from "react";
+
+ const Customer = () => {
+   const [customers, setCustomers] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+   const [currentPage, setCurrentPage] = useState(1);
+   const [customersPerPage] = useState(5); // Number of customers per page
+
+   useEffect(() => {
+     const fetchCustomers = async () => {
+       try {
+         const response = await fetch(
+           "https://comagency-assesmentyvette-serverside.onrender.com/api/v1/get-alll-customers"
+         );
+         if (!response.ok) {
+           throw new Error("Failed to fetch customers");
+         }
+         const data = await response.json();
+         setCustomers(data.customers); // Access 'customers' property
+       } catch (error) {
+         console.error("Error fetching data:", error);
+         setError(error.message);
+       } finally {
+         setLoading(false);
+       }
+     };
+
+     fetchCustomers();
+   }, []);
+
+   // Logic for pagination
+   const indexOfLastCustomer = currentPage * customersPerPage;
+   const indexOfFirstCustomer = indexOfLastCustomer - customersPerPage;
+   const currentCustomers = customers.slice(
+     indexOfFirstCustomer,
+     indexOfLastCustomer
+   );
+
+   // Change page
+   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+   if (loading) {
+     return <p>Loading data...</p>;
+   }
+
+   if (error) {
+     return <p>Error: {error}</p>;
+   }
+
+   return (
+     <div className="container mx-auto mt-10">
+       <h2 className="text-3xl font-bold text-center mb-4">
+         Customer Information
+       </h2>
+       <table className=" text-xs border-collapse border bg-opacity-55 bg-lime-600">
+         <thead>
+           <tr>
+             <th className="border py-2 px-4">Full Name</th>
+             <th className="border py-2 px-4">Age</th>
+             <th className="border py-2 px-4">Gender</th>
+             <th className="border py-2 px-4">Nationality</th>
+             <th className="border py-2 px-4">Identification</th>
+             <th className="border py-2 px-4">Passport Number</th>
+             <th className="border py-2 px-4">Purpose of Visit</th>
+             <th className="border py-2 px-4">Duration of Stay</th>
+             <th className="border py-2 px-4">Date of Entry</th>
+             <th className="border py-2 px-4">Port of Entry</th>
+             <th className="border py-2 px-4">Email Address</th>
+             <th className="border py-2 px-4">Phone Number</th>
+           </tr>
+         </thead>
+         <tbody>
+           {currentCustomers.length > 0 ? (
+             currentCustomers.map((customer) => (
+               <tr key={customer._id}>
+                 <td className="border px-4 py-2">{customer.fullName}</td>
+                 <td className="border px-4 py-2">{customer.age}</td>
+                 <td className="border px-4 py-2">{customer.gender}</td>
+                 <td className="border px-4 py-2">{customer.nationality}</td>
+                 <td className="border px-4 py-2">{customer.identification}</td>
+                 <td className="border px-4 py-2">{customer.passport}</td>
+                 <td className="border px-4 py-2">{customer.purposeOfVisit}</td>
+                 <td className="border px-4 py-2">{customer.durationOfStay}</td>
+                 <td className="border px-4 py-2">{customer.dateOfEntry}</td>
+                 <td className="border px-4 py-2">{customer.portOfEntry}</td>
+                 <td className="border px-4 py-2">{customer.emailAddress}</td>
+                 <td className="border px-4 py-2">{customer.phoneNumber}</td>
+               </tr>
+             ))
+           ) : (
+             <tr>
+               <td colSpan="12" className="border px-4 py-2 text-center">
+                 No customers found
+               </td>
+             </tr>
+           )}
+         </tbody>
+       </table>
+       {/* Pagination */}
+       <div className="flex justify-center mt-4">
+         {Array.from(
+           { length: Math.ceil(customers.length / customersPerPage) },
+           (_, i) => (
+             <button
+               key={i}
+               onClick={() => paginate(i + 1)}
+               className="mx-1 px-3 py-1 bg-gray-200 rounded"
+             >
+               {i + 1}
+             </button>
+           )
+         )}
+       </div>
+     </div>
+   );
+ };
+
+ export default Customer;
---
+++ src/component/Form.jsx
@@ -0,0 +1,377 @@
+import React, { useState } from "react";
+import border from "../images/th.jpg";
+import { Link } from "react-router-dom";
+import Customer from "./Customer";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    age: "",
+    gender: "",
+    nationality: "",
+    nationalId: "",
+    passportNumber: "",
+    purposeOfVisit: "",
+    durationOfStay: "",
+    dateOfEntry: "",
+    portOfEntry: "",
+    emailAddress: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    return /^\d{10,}$/.test(phoneNumber);
+  };
+
+  const validateAge = (age) => {
+    return age >= 0 && age <= 120;
+  };
+
+  const validateDateOfEntry = (date) => {
+    const today = new Date();
+    const entryDate = new Date(date);
+    return entryDate <= today;
+  };
+
+  const validateNationalId = (nationalId) => {
+    return /^[A-Za-z0-9]{6,12}$/.test(nationalId);
+  };
+
+  const validatePassportNumber = (passportNumber) => {
+    return /^[A-Za-z0-9]{6,9}$/.test(passportNumber);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(formData.emailAddress)) {
+      alert("Invalid email format");
+      return;
+    }
+
+    if (!validatePhoneNumber(formData.phoneNumber)) {
+      alert("Phone number should be numeric and at least 10 digits");
+      return;
+    }
+
+    if (!validateAge(formData.age)) {
+      alert("Age should be a number between 0 and 120");
+      return;
+    }
+
+    if (!validateDateOfEntry(formData.dateOfEntry)) {
+      alert("Date of Entry should not be in the future");
+      return;
+    }
+
+    if (
+      formData.nationality === "Local" &&
+      !validateNationalId(formData.nationalId)
+    ) {
+      alert(
+        "National ID should be alphanumeric and between 6 to 12 characters"
+      );
+      return;
+    }
+
+    if (
+      formData.nationality === "Foreign" &&
+      !validatePassportNumber(formData.passportNumber)
+    ) {
+      alert(
+        "Passport Number should be alphanumeric and between 6 to 9 characters"
+      );
+      return;
+    }
+
+    // Submit form data
+    try {
+      const response = await fetch(
+        "https://comagency-assesmentyvette-serverside.onrender.com/api/v1/create-customer",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        alert("Customer created successfully!");
+      } else {
+        alert("Failed to create customer. Please try again.");
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="rounded-lg flex flex-col">
+        <div className="bg-slate-950">
+          <div className="flex justify-between ">
+            <h1 className="text-4xl font-bold text-center mt-10 ml-80 text-white">
+              Customs Form
+            </h1>
+            <Link to="/customer">
+              <div className="text-xl rounded-3xl bg-lime-800 w-72 h-10  font-bold text-center mt-12 hover:bg-lime-500  mr-40 text-white ">
+                CUSTOMER_IMFORMATION
+              </div>
+            </Link>
+          </div>
+          <div
+            style={{
+              backgroundImage: `url(${border})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "80vh",
+              width: "100%",
+            }}
+          >
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-4 mt-12 text-center"
+            >
+              <div className="space-y-4 mt-12 flex flex-row flex-wrap">
+                <div className="bg-slate-950 bg-opacity-65 rounded-lg mt-4 ml-10 md:w-1/2 lg:w-1/3">
+                  <h1 className="text-white text-2xl font-semibold">
+                    PERSONAL INFORMATION
+                  </h1>
+                  <div className="mt-4 ">
+                    <label htmlFor="fullName" className="text-white -ml-16">
+                      Full Name
+                    </label>
+                    <input
+                      className="ml-5 w-60 rounded-md  bg-gray-400 "
+                      type="text"
+                      id="fullName"
+                      name="fullName"
+                      value={formData.fullName}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="mt-4">
+                    <label htmlFor="age" className="text-white -ml-12">
+                      Age
+                    </label>
+                    <input
+                      className="ml-14 w-60 rounded-md  bg-gray-400"
+                      type="number"
+                      id="age"
+                      name="age"
+                      value={formData.age}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="mt-4">
+                    <label htmlFor="gender" className="text-white -ml-16">
+                      Gender
+                    </label>
+                    <select
+                      className="ml-12 w-60 rounded-md bg-gray-400"
+                      id="gender"
+                      name="gender"
+                      value={formData.gender}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="">Select Gender</option>
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+                  <div className="mt-4 flex">
+                    <label htmlFor="nationality" className="text-white">
+                      Nationality
+                    </label>
+                    <select
+                      className="ml-8 w-60 rounded-md  bg-gray-400"
+                      id="nationality"
+                      name="nationality"
+                      value={formData.nationality}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="" className="text-black">
+                        Select Nationality
+                      </option>
+                      <option value="Local" className="text-black">
+                        Local
+                      </option>
+                      <option value="Foreign" className="text-black">
+                        Foreign
+                      </option>
+                    </select>
+                  </div>
+                  {formData.nationality === "Local" ? (
+                    <div className="mt-4 flex">
+                      <label htmlFor="nationalId" className="text-white">
+                        National_ID_Number
+                      </label>
+                      <input
+                        className="ml-4 w-60 rounded-md  bg-gray-400"
+                        type="text"
+                        id="nationalId"
+                        name="nationalId"
+                        value={formData.nationalId}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  ) : (
+                    <div className="mt-4 flex">
+                      <label htmlFor="passportNumber" className="text-white">
+                        Passport_Number
+                      </label>
+                      <input
+                        className="ml-4 w-60 rounded-md  bg-gray-400"
+                        type="text"
+                        id="passportNumber"
+                        name="passportNumber"
+                        value={formData.passportNumber}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="bg-slate-950 bg-opacity-65 rounded-lg mb-6 ml-5 md:w-1/2 lg:w-1/3">
+                  <h6 className="text-white text-2xl font-semibold">
+                    TRAVEL INFORMATION
+                  </h6>
+                  <div className="flex justify-between mt-5 mr-5">
+                    <label htmlFor="purposeOfVisit" className="text-white">
+                      Purpose of Visit
+                    </label>
+                    <select
+                      className="ml-4 w-60 rounded-md  bg-gray-400"
+                      id="purposeOfVisit"
+                      name="purposeOfVisit"
+                      value={formData.purposeOfVisit}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="">Select Purpose</option>
+                      <option value="Tourism">Tourism</option>
+                      <option value="Business">Business</option>
+                      <option value="Education">Education</option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+                  <div className="flex mt-5 justify-between mr-5">
+                    <label htmlFor="durationOfStay" className="text-white">
+                      Duration of Stay
+                    </label>
+                    <input
+                      className="ml-4 w-60 rounded-md  bg-gray-400"
+                      type="number"
+                      id="durationOfStay"
+                      name="durationOfStay"
+                      value={formData.durationOfStay}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="flex justify-between mt-5 mr-5">
+                    <label htmlFor="dateOfEntry" className="text-white">
+                      Date of Entry
+                    </label>
+                    <input
+                      className="ml-4 w-60 rounded-md  bg-gray-400"
+                      type="date"
+                      id="dateOfEntry"
+                      name="dateOfEntry"
+                      value={formData.dateOfEntry}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="flex justify-between m-5">
+                    <label
+                      htmlFor="portOfEntry"
+                      className=" text-white w-40 -ml-10"
+                    >
+                      Port of Entry
+                    </label>
+                    <select
+                      className=" w-72 rounded-md ml-14  bg-gray-400"
+                      id="portOfEntry"
+                      name="portOfEntry"
+                      value={formData.portOfEntry}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="">Select Port</option>
+                      <option value="Port A">Gatuna</option>
+                      <option value="Port B">Kagitumba</option>
+                      <option value="Port C">Cyanika</option>
+                      <option value="Port D">Rusumo</option>
+                    </select>
+                  </div>
+                  <h6 className="text-white text-2xl font-semibold">
+                    CONTACT INFORMATION
+                  </h6>
+                  <div className="flex justify-between m-5">
+                    <label htmlFor="email" className="text-white -ml-5">
+                      Emailadress
+                    </label>
+                    <input
+                      className="w-60 rounded-md  bg-gray-400"
+                      type="email"
+                      id="emailAddress"
+                      name="emailAddress"
+                      value={formData.emailAddress}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="flex justify-between m-5">
+                    <label htmlFor="phoneNumber" className="text-white -ml-5">
+                      Phone Number
+                    </label>
+                    <input
+                      className="w-60 rounded-md  bg-gray-400"
+                      type="text"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      value={formData.phoneNumber}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="bg-teal-900 hover:bg-teal-400 text-black mr-56 font-bold  md:w-48 h-10 rounded-md"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
